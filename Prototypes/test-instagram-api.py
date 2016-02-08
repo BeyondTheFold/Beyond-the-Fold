542935930b09fa55bd452eba22e0d5f3d71dadb4
Drew Gunderson <drewgun@gmail.com>
2016-02-07 22:58:35 -0500

    - Created initial project structure

---
+++ Prototypes/test-instagram-api.py
@@ -0,0 +1,14 @@
+import instagram
+
+access_token = "e396c177fc424d24bdd0c0a3c7539c79"
+client_secret = "e396c177fc424d24bdd0c0a3c7539c79"
+
+
+'''
+api = InstagramAPI(access_token=access_token, client_secret=client_secret)
+
+recent_media, next_ = api.user_recent_media(user_id="drewgun", count=10)
+
+for media in recent_media:
+    print(media.caption.text)
+'''
